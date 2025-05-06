7f472f09f469925a31d25bd7d1d9d49212cb7be2
Aarush Jain <aarushjain33555@gmail.com>
2025-05-05 22:56:20 -0400

    added function

--- Frontend/lexer.cpp
+++ Frontend/lexer.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -15,12 +16,41 @@ enum class TokenType {
     COMMENT,
 };
 
-struct Token {
-    TokenType token;
+class Lexer {
+
+    public:
+        std::unordered_map<std::string, bool> keywords;
+        
+        Lexer() {
+            keywords["Let"] = true;
+            keywords["be"] = true;
+            keywords["set_breakpoint"] = true;
+            keywords["From"] = true;
+            keywords["until"] = true;
+            keywords["by"] = true;
+            keywords["is"] = true;
+            keywords["rewind"] = true;
+        }
+
 };
 
-void read_state(std::vector<char> characters) {
-     
+struct Token {
+    TokenType token;
+    std::string value;
+};
+
+// Pseudocode:
+// read_state function receives current state and character vector
+// 
+
+
+
+void read_state(std::vector<char> characters, TokenType *current_state, Lexer object) {
+    if (characters.at(characters.size() - 1) == ' ') {
+        for (int i = 0; i < characters.size() - 1; ++i) {
+            if (characters.at(i) == "L"
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -30,13 +60,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     {
+
         std::ifstream file(argv[1]);
         std::string line;
         std::vector<char> characters;
+        Lexer obj1 = Lexer();
+        TokenType initial_state = TokenType::IDLE;
+        TokenType* current_state = &initial_state;
         while (std::getline(file, line)) {
             for (char ch : line) {
                 characters.push_back(ch);
-                read_state(characters);
+                read_state(characters, current_state, obj1);
 
             }
         }
