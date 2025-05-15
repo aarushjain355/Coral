f118f407d4ab5e9d008bcb0e01e8640057c26197
Aarush Jain <aarushjain33555@gmail.com>
2025-05-14 21:32:39 -0400

    Fixed formatting

--- Frontend/lexer.cpp
+++ Frontend/lexer.cpp
@@ -21,7 +21,7 @@ enum class TokenType {
 };
 
 // TOOK CARE OF KEYWORD, ERROR, IDENTIFIER, IDLE, OPERATOR
-// Need to take care of number, float, operator, and comment 
+// Need to take care of number, float, operator, and comment
 
 class Lexer {
 
@@ -53,15 +53,16 @@ struct Token {
 void read_string_state(std::vector<char> characters, TokenType *current_state, Lexer object) {
 
     std::string result = "";
-    if (object.operators_.find(characters.at(0)) != object.operators_.end()) {
+    if (characters.size() > 0 && object.operators_.find(characters.at(0)) != object.operators_.end()) {
         *current_state = TokenType::OPERATOR; 
         object.token_list_.insert({*current_state, std::string(1, characters.at(0))});
-        characters.clear();
+        
 
     } else {
         // Add in comment code somewhere here
         for (int i = 0; i < characters.size()-1; ++i) {
             if (!std::isalpha(characters.at(i))) {
+                // write code to detect integer vs a double
                 *current_state = TokenType::ERROR;
                 object.token_list_.insert({*current_state, result});
                 break;
@@ -75,13 +76,12 @@ void read_string_state(std::vector<char> characters, TokenType *current_state, L
         }
         object.token_list_.insert({*current_state, result});
     }
+    characters.clear();
    
     
 }
 
-void read_numerical_state(std::vector<char> characters, TokenType *current_state, Lexer object) {
-    characters.push_back()
-}
+
 
 int main(int argc, char* argv[]) {
 
@@ -100,10 +100,16 @@ int main(int argc, char* argv[]) {
             for (char ch : line) {
                 if (ch == ' ') {
                     read_string_state(characters, current_state, obj1);
-                } else if (std::isdigit(ch)) {
+                } else if (ch == '.') {
+                    if (characters.size() != 0 && characters.at(characters.size()-1).isdigit()) {
+
+                    }
                     read_numerical_state(characters, current_state, obj1);
                     // Add code for detecting a float or a double
-                }    
+                } else {
+                    characters.push_back(ch);
+                    
+                }  
             }
         }
     }
